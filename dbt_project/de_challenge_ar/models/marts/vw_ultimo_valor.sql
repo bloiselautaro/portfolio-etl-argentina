12133657b65d5cd9ef8d4394f2e1f0f0cd9caa7e
bloiselautaro <laubloise@gmail.com>
2026-07-12 17:15:55 -0300

    feat: agregar inflacion acumulada 12m a vw_ultimo_valor

--- dbt_project/de_challenge_ar/models/marts/vw_ultimo_valor.sql
+++ dbt_project/de_challenge_ar/models/marts/vw_ultimo_valor.sql
@@ -18,6 +18,12 @@ qualify row_number() over (order by fecha desc) = 1
 
 union all
 
+select 'inflacion_acumulada_12m_pct', fecha, inflacion_acumulada_12m_pct
+from {{ ref('fct_inflacion') }}
+qualify row_number() over (order by fecha desc) = 1
+
+union all
+
 select 'reservas_millones_usd', fecha, reservas_millones_usd
 from {{ ref('fct_reservas_bcra') }}
 qualify row_number() over (order by fecha desc) = 1
@@ -27,9 +33,3 @@ union all
 select 'tasa_politica_monetaria_pct', fecha, tasa_politica_monetaria_pct
 from {{ ref('fct_tasa_politica_monetaria') }}
 qualify row_number() over (order by fecha desc) = 1
-
-union all
-
-select concat('dolar_', tipo_dolar) as indicador, fecha, precio_venta as valor
-from {{ ref('stg_dolar') }}
-qualify row_number() over (partition by tipo_dolar order by fecha desc) = 1
