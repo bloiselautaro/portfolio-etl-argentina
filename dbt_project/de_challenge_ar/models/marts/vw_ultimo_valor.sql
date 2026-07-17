f0ff3124e1677a0679ccc9fe564eeffca0c9513e
bloiselautaro <laubloise@gmail.com>
2026-07-17 19:49:53 -0300

    Unificar vw_ultimo_valor_dolar en vw_ultimo_valor

--- dbt_project/de_challenge_ar/models/marts/vw_ultimo_valor.sql
+++ dbt_project/de_challenge_ar/models/marts/vw_ultimo_valor.sql
@@ -1,5 +1,17 @@
 {{ config(materialized='view') }}
 
+with ultimo_dolar as (
+
+    select
+        tipo_dolar,
+        fecha,
+        precio_venta,
+        variacion_desde_ultimo_cambio_pct
+    from {{ ref('fct_dolar') }}
+    qualify row_number() over (partition by tipo_dolar order by fecha desc) = 1
+
+)
+
 select 'riesgo_pais' as indicador, fecha, cast(riesgo_pais as float64) as valor
 from {{ ref('fct_riesgo_pais') }}
 qualify row_number() over (order by fecha desc) = 1
@@ -45,3 +57,53 @@ union all
 select 'tasa_politica_monetaria_pct', fecha, tasa_politica_monetaria_pct
 from {{ ref('fct_tasa_politica_monetaria') }}
 qualify row_number() over (order by fecha desc) = 1
+
+union all
+
+select 'dolar_oficial_precio', fecha, precio_venta
+from ultimo_dolar where tipo_dolar = 'oficial'
+
+union all
+
+select 'dolar_oficial_variacion_pct', fecha, variacion_desde_ultimo_cambio_pct
+from ultimo_dolar where tipo_dolar = 'oficial'
+
+union all
+
+select 'dolar_blue_precio', fecha, precio_venta
+from ultimo_dolar where tipo_dolar = 'blue'
+
+union all
+
+select 'dolar_blue_variacion_pct', fecha, variacion_desde_ultimo_cambio_pct
+from ultimo_dolar where tipo_dolar = 'blue'
+
+union all
+
+select 'dolar_ccl_precio', fecha, precio_venta
+from ultimo_dolar where tipo_dolar = 'contadoconliqui'
+
+union all
+
+select 'dolar_ccl_variacion_pct', fecha, variacion_desde_ultimo_cambio_pct
+from ultimo_dolar where tipo_dolar = 'contadoconliqui'
+
+union all
+
+select 'dolar_mep_precio', fecha, precio_venta
+from ultimo_dolar where tipo_dolar = 'bolsa'
+
+union all
+
+select 'dolar_mep_variacion_pct', fecha, variacion_desde_ultimo_cambio_pct
+from ultimo_dolar where tipo_dolar = 'bolsa'
+
+union all
+
+select 'dolar_mayorista_precio', fecha, precio_venta
+from ultimo_dolar where tipo_dolar = 'mayorista'
+
+union all
+
+select 'dolar_mayorista_variacion_pct', fecha, variacion_desde_ultimo_cambio_pct
+from ultimo_dolar where tipo_dolar = 'mayorista'
--- dbt_project/de_challenge_ar/models/marts/vw_ultimo_valor_dolar.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ ref('fct_dolar') }}
-qualify row_number() over (partition by tipo_dolar order by fecha desc) = 1
